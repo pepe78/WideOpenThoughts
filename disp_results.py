4942e7a028b766309c473ea335f73fe7f474823f
Peter Taraba <taraba.peter@mail.com>
2020-05-30 17:19:04 -0700

    improvement

--- disp_results.py
+++ disp_results.py
@@ -3,6 +3,16 @@ import sys
 
 filename = 'debug.csv'
 
+def addsubgraph(which, t, q1, q2, lab):
+	plt.subplot(which)
+	plt.plot(t,q1,'b', label='train')
+	plt.plot(t,q2,'r', label='test')
+	plt.xlabel('epoch')
+	plt.ylabel(lab)
+	plt.grid(color='k', linestyle=':', linewidth=1)
+	plt.legend(bbox_to_anchor=(0., 1.02, 1., .102), loc='lower left',
+		   ncol=2, mode="expand", borderaxespad=0.)
+
 t=[]
 e1=[]
 a1=[]
@@ -27,16 +37,12 @@ with open(filename, 'rt') as f:
 print('Max train accuracy:', max(a1))
 print('Max test accuracy:', max(a2))
 
-plt.subplot(211)
-plt.plot(t,a1,'b')
-plt.plot(t,a2,'r')
+plt.figure(figsize=(30,20))
 
-plt.subplot(212)
-plt.plot(t,e1,'b')
-plt.plot(t,e2,'r')
-
+addsubgraph(221,t,a1,a2,'accuracy')
+addsubgraph(223,t,e1,e2,'error')
+addsubgraph(222,t[-len(t)//2:],a1[-len(t)//2:],a2[-len(t)//2:],'accuracy')
+addsubgraph(224,t[-len(t)//2:],e1[-len(t)//2:],e2[-len(t)//2:],'error')
+           
 plt.show()
-
-#plt.imshow(m)
-#plt.show()
 
